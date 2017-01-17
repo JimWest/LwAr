8d8689f61021fa517e55cd60c853d0f8ea43d947
JimWest <j1m_w3st@web.de>
2017-01-17 21:08:34 +0100

    project cleanup

--- Source/LwAR/ExternalLibs.h
+++ Source/LwAR/ExternalLibs.h
@@ -2,22 +2,10 @@
 // Loads all the needed libs depending on the configuration (Debug or Release)
 
 #ifdef _DEBUG 
-/*
-#pragma comment(lib, "opencv_calib3d2413d.lib")
-#pragma comment(lib, "opencv_core2413d.lib")
-#pragma comment(lib, "opencv_highgui2413d.lib")
-#pragma comment(lib, "opencv_imgproc2413d.lib")
-#pragma comment(lib, "opencv_objdetect2413d.lib")*/
 #pragma comment(lib, "opencv_world310d.lib")
 
 
 #else
-/*
-#pragma comment(lib, "opencv_calib3d2413.lib")
-#pragma comment(lib, "opencv_core2413.lib")
-#pragma comment(lib, "opencv_highgui2413.lib")
-#pragma comment(lib, "opencv_imgproc2413.lib")
-#pragma comment(lib, "opencv_objdetect2413.lib")*/
 #pragma comment(lib, "opencv_world310d.lib")
 #endif
 
