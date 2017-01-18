68a18a73563b215149e652f3b4414fffd8d357fd
JimWest <j1m_w3st@web.de>
2017-01-18 14:02:33 +0100

    removed un used aruco files

--- Source/LwAR/ExternalLibs.h
+++ Source/LwAR/ExternalLibs.h
@@ -3,14 +3,13 @@
 
 #ifdef _DEBUG 
 #pragma comment(lib, "opencv_world310d.lib")
-#pragma comment(lib, "aruco201.libd")
 
 #else
 #pragma comment(lib, "opencv_world310.lib")
-#pragma comment(lib, "aruco201.lib")
 #endif
 
 #pragma comment(lib, "opengl32.lib")
 #pragma comment(lib, "glu32.lib")
 #pragma comment(lib, "glew32.lib")
 #pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "aruco201.lib")
